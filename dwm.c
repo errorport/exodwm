3f2519899e8c71600debc0022ddfec1166377371
Gergely Bencsik <becsegergely@gmail.com>
2019-06-03 00:43:34 +0200

    Animgif bg added.

--- dwm.c
+++ dwm.c
@@ -28,6 +28,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h> 
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
@@ -2011,7 +2012,7 @@ updatestatus(void)
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext))) {
 //		strcpy(stext, "dwm-"VERSION);
 //		strcpy(stext, " ");
-		strcpy(stext, " ");
+		strcpy(stext, "");
 //		strcpy(stext, ctime(&rawtime));
 	}
 	drawbar(selmon);
@@ -2164,6 +2165,8 @@ main(int argc, char *argv[])
 		die("dwm: cannot open display");
 	checkotherwm();
 	setup();
+	system("~/gifbg.sh ~/gifbg/001.gif &");
+	system("~/dev/dwm/exostatus.sh &");
 #ifdef __OpenBSD__
 	if (pledge("stdio rpath proc exec", NULL) == -1)
 		die("pledge");
---
+++ exostatus.sh
@@ -0,0 +1,14 @@
+exostat_path="$HOME/dev/exostat/exostat"
+
+_text=""
+
+update_text()
+{
+	_text=`$exostat_path`
+}
+
+while true; do
+	update_text
+	xsetroot -name "$_text"
+	sleep 1
+done
